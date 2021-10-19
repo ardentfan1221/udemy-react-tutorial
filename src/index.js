a4ab90a0f33b6b635f6a090cf1c8412b4cd7611a
ardentfan1221 <75660085+ardentfan1221@users.noreply.github.com>
2021-10-19 11:18:46 +0530

    Create 2 different props for displaying 2 different books

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import ReactDom from 'react-dom'
+import './index.css'
+
+const firstBook = {
+	img: 'https://images-eu.ssl-images-amazon.com/images/I/41cWqh0OeQL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
+	title: 'The Psychology of Money',
+	author: 'Morgan Housel',
+}
+
+const secondBook = {
+	img: 'https://images-na.ssl-images-amazon.com/images/I/41aO+dGmtQL._SX329_BO1,204,203,200_.jpg',
+	title: 'Peril',
+	author: 'Bob Woodward, Robert Costa',
+}
+
+function BookList() {
+	return (
+		<section className='booklist'>
+			<Book
+				img={firstBook.img}
+				title={firstBook.title}
+				author={firstBook.author}
+			/>
+
+			<Book
+				img={secondBook.img}
+				title={secondBook.title}
+				author={secondBook.author}
+			/>
+		</section>
+	)
+}
+
+const Book = (props) => {
+	return (
+		<article className='book'>
+			<img src={props.img} alt='' />
+			<h2>{props.title}</h2>
+			<h4>{props.author}</h4>
+		</article>
+	)
+}
+
+ReactDom.render(<BookList />, document.getElementById('root'))
