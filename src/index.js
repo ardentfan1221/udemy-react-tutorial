67c34f00d9a85eccd1bd722c51350d5fa66064d6
ardentfan1221 <75660085+ardentfan1221@users.noreply.github.com>
2021-10-19 13:58:30 +0530

    Add Books to an array

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 import ReactDom from 'react-dom'
 import './index.css'
 
-const firstBook = {
-	img: 'https://images-eu.ssl-images-amazon.com/images/I/41cWqh0OeQL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
-	title: 'The Psychology of Money',
-	author: 'Morgan Housel',
-}
-
-const secondBook = {
-	img: 'https://images-na.ssl-images-amazon.com/images/I/41aO+dGmtQL._SX329_BO1,204,203,200_.jpg',
-	title: 'Peril',
-	author: 'Bob Woodward, Robert Costa',
-}
+const books = [
+	{
+		img: 'https://images-eu.ssl-images-amazon.com/images/I/41cWqh0OeQL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
+		title: 'The Psychology of Money',
+		author: 'Morgan Housel',
+	},
+	{
+		img: 'https://images-na.ssl-images-amazon.com/images/I/41aO+dGmtQL._SX329_BO1,204,203,200_.jpg',
+		title: 'Peril',
+		author: 'Bob Woodward, Robert Costa',
+	},
+]
 
 function BookList() {
 	return (
 		<section className='booklist'>
 			<Book
-				img={firstBook.img}
-				title={firstBook.title}
-				author={firstBook.author}
+				img={books[0].img}
+				title={books[0].title}
+				author={books[0].author}
 			/>
 
 			<Book
-				img={secondBook.img}
-				title={secondBook.title}
-				author={secondBook.author}
+				img={books[1].img}
+				title={books[1].title}
+				author={books[1].author}
 			/>
 		</section>
 	)
