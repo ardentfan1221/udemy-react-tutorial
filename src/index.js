fab28e9029aaef20047c12cb63bab2c8b11e4ebd
ardentfan1221 <75660085+ardentfan1221@users.noreply.github.com>
2021-10-19 14:19:48 +0530

    Add spread operator to props

--- src/index.js
+++ src/index.js
@@ -3,14 +3,22 @@ import './index.css'
 
 const books = [
 	{
+		id: 1,
 		img: 'https://images-eu.ssl-images-amazon.com/images/I/41cWqh0OeQL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
 		title: 'The Psychology of Money',
 		author: 'Morgan Housel',
 	},
 	{
+		id: 2,
 		img: 'https://images-na.ssl-images-amazon.com/images/I/41aO+dGmtQL._SX329_BO1,204,203,200_.jpg',
 		title: 'Peril',
 		author: 'Bob Woodward, Robert Costa',
+	},
+	{
+		id: 3,
+		img: 'https://images-na.ssl-images-amazon.com/images/I/51CPNIRSWeL._AC_SX184_.jpg',
+		title: 'Atomic Habits',
+		author: 'James Clear',
 	},
 ]
 
@@ -18,14 +26,14 @@ function BookList() {
 	return (
 		<section className='booklist'>
 			{books.map((book) => {
-				return <Book book={book} />
+				return <Book key={book.id} {...book} />
 			})}
 		</section>
 	)
 }
 
 const Book = (props) => {
-	const { img, title, author } = props.book
+	const { img, title, author } = props
 	return (
 		<article className='book'>
 			<img src={img} alt='' />
